2a20705d4a8276ed68a8a4ca35b4ea33071df10b
Draldo <draldo_lopez@hotmail.com>
2016-08-10 12:52:49 -0400

    Google map and get coordinates

--- app/src/main/java/com/example/simplemap/MainActivity.java
+++ app/src/main/java/com/example/simplemap/MainActivity.java
@@ -1,9 +1,19 @@
 package com.example.simplemap;
 
-import android.support.v7.app.AppCompatActivity;
+
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -11,25 +21,84 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback,
+        GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
 
     private static final String TAG = "MainActivityTAG_";
+    private GoogleApiClient mGoogleApi;
+    private double mLatitud;
+    private double mLongitud;
+    private LocationListener mLocationListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mGoogleApi = buildGoogleApiClient();
+        mGoogleApi.connect();
+
+        mLocationListener = new LocationListener() {
+
+            @Override
+            public void onLocationChanged(Location location) {
+                mLatitud = location.getLatitude();
+                mLongitud = location.getLongitude();
+                Log.d(TAG, "onLocationChanged: " + mLatitud + " " + mLongitud);
+            }
+
+            @Override
+            public void onStatusChanged(String s, int i, Bundle bundle) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String s) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String s) {
+
+            }
+        };
+
+        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 2000, 10, mLocationListener);
+
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.a_main_maps);
         mapFragment.getMapAsync(this);
     }
 
+    protected synchronized GoogleApiClient buildGoogleApiClient() {
+        return new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+    }
+
     @Override
     public void onMapReady(GoogleMap map) {
-
         Log.d(TAG, "onMapReady: ");
-        LatLng sydney = new LatLng(-34, 151);
-        map.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        map.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng mexico = new LatLng(19.4326077, -99.13320799999997);
+        map.addMarker(new MarkerOptions().position(mexico).title("Marker in Mexico"));
+        map.moveCamera(CameraUpdateFactory.newLatLng(mexico));
     }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
 }
